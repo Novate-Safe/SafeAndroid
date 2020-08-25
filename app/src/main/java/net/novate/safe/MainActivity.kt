b5e0a54a6b6ef6ccdf8024e0fd51fb77d4d2fbdd
GavinNovate <gavin.novate@gmail.com>
2020-08-25 23:48:59 +0800

    引入导航组件

--- app/src/main/java/net/novate/safe/MainActivity.kt
+++ app/src/main/java/net/novate/safe/MainActivity.kt
@@ -2,6 +2,9 @@ package net.novate.safe
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.main_activity.*
 import net.novate.base.binding.bindContentView
 import net.novate.base.ui.system.appearBehindNavigationBar
 import net.novate.base.ui.system.appearBehindStatusBar
@@ -17,18 +20,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = bindContentView(R.layout.main_activity)
 
-        supportFragmentManager.beginTransaction().replace(R.id.container, HomeFragment()).commit()
 
-        binding.navigation.setOnNavigationItemSelectedListener {
-            supportFragmentManager.beginTransaction().replace(
-                R.id.container, when (it.itemId) {
-                    R.id.home -> HomeFragment()
-                    R.id.dynamic -> DynamicFragment()
-                    R.id.mine -> MineFragment()
-                    else -> throw UnsupportedOperationException("")
-                }
-            ).commit()
-            true
-        }
+        navigationView.setupWithNavController(navigationHost.findNavController())
+//        navigationHost.findNavController()
+//        supportFragmentManager.beginTransaction().replace(R.id.container, HomeFragment()).commit()
+//
+//        binding.navigation.setOnNavigationItemSelectedListener {
+//            supportFragmentManager.beginTransaction().replace(
+//                R.id.container, when (it.itemId) {
+//                    R.id.home -> HomeFragment()
+//                    R.id.dynamic -> DynamicFragment()
+//                    R.id.mine -> MineFragment()
+//                    else -> throw UnsupportedOperationException("")
+//                }
+//            ).commit()
+//            true
+//        }
     }
 }
