5505e0bf0342e28ca7228b45638bc8afd938dc7a
GavinNovate <gavin.novate@gmail.com>
2020-10-29 00:59:58 +0800

    添加暗色主题部分配色

--- app/src/main/java/net/novate/safe/ui/NavigationActivity.kt
+++ app/src/main/java/net/novate/safe/ui/NavigationActivity.kt
@@ -1,11 +1,16 @@
 package net.novate.safe.ui
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.plus
 import androidx.navigation.findNavController
 import net.novate.base.ui.system.appearBehindNavigationBar
 import net.novate.base.ui.system.appearBehindStatusBar
 import net.novate.safe.R
+import okio.Buffer
+import java.util.*
 
 /**
  * 导航页
@@ -19,6 +24,16 @@ class NavigationActivity : AppCompatActivity() {
         window.appearBehindNavigationBar = true
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_activity)
+
+        c()
     }
 
 }
+
+// 颜色计算小工具
+fun c() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        println("#${Buffer().writeInt((Color.valueOf(Color.parseColor("#121212")).plus(Color.valueOf(Color.parseColor("#1FFFFFFF")))).toArgb()).readByteString().hex().toUpperCase(Locale.getDefault())}")
+    }
+}
+
